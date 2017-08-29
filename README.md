b43039dcaa0212f2748d3b00dc9e8c5c1d21fd77
Timur Jaganov <andeniel@gmail.com>
2017-08-29 10:05:45 +0600

    completed task

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
-# Frequency Analysis of Words
+# Анализ частоты слов
 
-[TODO. There will be project description]
+Скрипт позволяет подсчитать количество слов в тексте и выводит в консоль десять самых популярных слов в порядке убывания частоты. 
 
-# Project Goals
+# Как запустить
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+
+Запуск на Linux:
+
+```#!bash
+# Программа принимает 3 параметра
+# python3 lang_frequency.py -f [путь к файлу]
+# -f [указываем путь к текстовому файлу]
+
+$ python3 lang_frequency.py -f ru_sample.txt
+# пример ответа скрипта
+Часто используемые слова в тексте:
+что     3646
+как     1913
+его     1876
+она     1195
+сказал  1145
+было    1137
+это     1030
+так     952
+ему     882
+всё     839
+
+```
+
+Запуск на Windows происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке ― [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,28 @@
+import argparse
+import io
+import re
+from collections import Counter
+
+def load_data(file_path):    
+    with io.open(file_path, "r", encoding="utf8") as data_file:
+        return data_file.read()
 
 
-def load_data(filepath):
-    pass
-
-
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(data_text):
+    reg = re.compile(r'\W')
+    words = list(map(lambda word: reg.sub('', word), data_text.split()))    
+    clear_words = list(filter(lambda word: len(word) > 2, words))    
+    count_words = Counter(clear_words)
+    sorted_words = sorted(count_words.items(), key=lambda item:item[1], reverse=True)
+    return sorted_words[:10]    
 
 
 if __name__ == '__main__':
-    pass
+    aparser = argparse.ArgumentParser()
+    aparser.add_argument("-f", "--file", required=True, help="Filepath")
+    args = vars(aparser.parse_args())
+    data_text = load_data(args['file'])    
+    frequent_words = get_most_frequent_words(data_text)
+    print("Часто используемые слова в тексте:")
+    for item in frequent_words:
+        print("{}\t{}".format(*item))        
