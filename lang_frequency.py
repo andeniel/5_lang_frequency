1b9c761d7ae22d266578099b4d6f1c0e80d6f954
Timur Jaganov <andeniel@gmail.com>
2017-08-29 13:15:16 +0600

    fixed comments

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,26 +3,28 @@ import io
 import re
 from collections import Counter
 
-def load_data(file_path):    
+
+def load_data(file_path):
     with io.open(file_path, "r", encoding="utf8") as data_file:
         return data_file.read()
 
 
 def get_most_frequent_words(data_text):
     reg = re.compile(r'\W')
-    words = list(map(lambda word: reg.sub('', word), data_text.split()))    
-    clear_words = list(filter(lambda word: len(word) > 2, words))    
+    words = list(map(lambda word: reg.sub('', word), data_text.split()))
+    clear_words = list(filter(lambda word: len(word) > 2, words))
     count_words = Counter(clear_words)
-    sorted_words = sorted(count_words.items(), key=lambda word:word[1], reverse=True)
-    return sorted_words[:10]    
+    sorted_words = sorted(
+        count_words.items(), key=lambda word: word[1], reverse=True)
+    return sorted_words[:10]
 
 
 if __name__ == '__main__':
     aparser = argparse.ArgumentParser()
     aparser.add_argument("-f", "--file", required=True, help="Filepath")
     args = vars(aparser.parse_args())
-    data_text = load_data(args['file'])    
+    data_text = load_data(args['file'])
     frequent_words = get_most_frequent_words(data_text)
     print("Часто используемые слова в тексте:")
     for word in frequent_words:
-        print("{}\t{}".format(*word))        
+        print("{}\t{}".format(*word))
